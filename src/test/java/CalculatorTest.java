2441b505dec75599257980f429273e51ec8a410d
intaeHwang <hwang9029@ckinform.com>
2025-09-20 22:20:14 +0900

    테스트 추가

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,6 +8,8 @@ public class CalculatorTest {
     @Test
     void plus() {
         int result = Calculator.plus(1, 2);
+
         assertThat(result).isEqualTo(3);
+        assertThat(Calculator.plus(4, 1)).isEqualTo(5);
     }
 }
