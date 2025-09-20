881b0508e0cbd9db8bb67b896bbf790ecaf30479
intaeHwang <hwang9029@ckinform.com>
2025-09-20 22:17:44 +0900

    Calculator가 실행될 정도만 개발

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,13 @@
+import intae.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+    
+    @Test
+    void plus() {
+        int result = Calculator.plus(1, 2);
+        assertThat(result).isEqualTo(3);
+    }
+}
