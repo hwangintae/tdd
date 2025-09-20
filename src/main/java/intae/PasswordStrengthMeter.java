7c7e1c5bb175c2b4a92d8dc7cb871c9d40ecee79
intaeHwang <hwang9029@ckinform.com>
2025-09-20 23:13:16 +0900

    길이가 8글자 미만인 경우 개발

--- src/main/java/intae/PasswordStrengthMeter.java
+++ src/main/java/intae/PasswordStrengthMeter.java
@@ -3,6 +3,10 @@ package intae;
 public class PasswordStrengthMeter {
 
     public PasswordStrength meter(String password) {
+        if (password.length() < 8) {
+            return PasswordStrength.NORMAL;
+        }
+
         return PasswordStrength.STRONG;
     }
 }
