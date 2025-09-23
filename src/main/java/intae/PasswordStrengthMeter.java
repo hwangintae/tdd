c2e748b55cc00884a657ce47129e2154d447e1b3
intaeHwang <hwang9029@ckinform.com>
2025-09-23 23:34:32 +0900

    숫자를 포함하지 않고 나머지 조건은 충족하는 경우 개발

--- src/main/java/intae/PasswordStrengthMeter.java
+++ src/main/java/intae/PasswordStrengthMeter.java
@@ -7,6 +7,15 @@ public class PasswordStrengthMeter {
             return PasswordStrength.NORMAL;
         }
 
+        boolean containsNum = false;
+        for (char ch : password.toCharArray()) {
+            if (ch >= '0' && ch <= '9') {
+                containsNum = true;
+                break;
+            }
+        }
+        if (!containsNum) return PasswordStrength.NORMAL;
+
         return PasswordStrength.STRONG;
     }
 }
